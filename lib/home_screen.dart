3e249b1bc710efc94de0af3f7aac1174da6edae4
shahrier <hossainshahrier.shm@gmail.com>
2022-11-15 18:36:53 +0600

    day2

---
+++ lib/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  final int days = 30;
+  final String name = "flutter";
+
+  const HomeScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Catalog app"),
+      ),
+      drawer: const Drawer(),
+      body: Center(
+        child: Text("Welcome to $days days of $name"),
+      ),
+    );
+  }
+}
